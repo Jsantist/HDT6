1c5b3b3b5a0c05bea2146c8909adf50bfe23010d
jsantis <san21153@uvg.edu.gt>
2022-03-21 22:09:18 -0600

    Construcción de display de usuario

--- HDT6/src/hdt6/Controlador.java
+++ HDT6/src/hdt6/Controlador.java
@@ -12,17 +12,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  *
  * @author jsken
  */
-public class Controlador {
+public class Controlador     {
     FileReader leer;
     FileWriter escribir;
     PrintWriter linea;
     BufferedReader almacenamiento;
+
+    public IMap<String, List<String>> getCollection() {
+        return collection;
+    }
+
+    public IMap<String, List<String>> getInventary() {
+        return inventary;
+    }
     
     String a = "hola";
     
@@ -79,11 +88,11 @@ public class Controlador {
     
     
     public void Factory() throws IOException{
-        System.out.println(" :: TIENDA ONLINE ::");
+        
 
         // Implementar patron de diseño Factory para seleccionar que tipo de MAP
         // utilizara
-        System.out.println("Ingrese que MAP implementara: ");
+        System.out.println("Ingrese tipo de mapa a implementar: ");
         System.out.println("1. HashMAP");
         System.out.println("2. TreeMAP");
         System.out.println("3. LinkedHashMap");
@@ -94,6 +103,124 @@ public class Controlador {
         inventary = factoryMap.getMap(optionMAP);
         
     }
+    
+    public void addProductToCollection() throws IOException {
+
+        Boolean isActive = true;
+
+        while (isActive) {
+
+            System.out.println("\n :: AGREGAR PRODUCTOS A LA COLECCION ::");
+
+            System.out.print("\n-> Ingrese el nombre de una category (X -> salir): ");
+            String category = br.readLine().toUpperCase();
+
+            if (category.equals("X")) {
+                isActive = false;
+                break;
+
+            } else {
+
+                List<String> products = inventary.get(category);
+
+                if (products == null) {
+                    System.out.println("[!] La categoria que desea ingresar no existe");
+                } else {
+
+                    for (int i = 0; i < products.size(); i++) {
+                        System.out.println((i + 1) + ". " + products.get(i));
+                    }
+
+                    System.out.print("\n-> Ingrese el numero de un producto ");
+                    int productIndex = Integer.parseInt(br.readLine());
+                    productIndex = productIndex - 1;
+
+                    if (productIndex > products.size() || productIndex < 1) {
+                        System.out.println("\n[!] No existe ese producto");
+                    } else {
+
+                        // Revisa si existe la categoria, si no existe la creo
+                        if (collection.get(category) != null) {
+
+                            List<String> currentList = collection.get(category);
+                            currentList.add(products.get(productIndex));
+                            collection.put(category, currentList);
+
+                        } else {
+                            List<String> newList = new ArrayList<String>();
+                            newList.add(products.get(productIndex));
+                            collection.put(category, newList);
+                        }
+
+                        System.out.println("\n[OK] Producto agregado exitosamente");
+
+                    }
+
+                }
+
+            }
+        }
+
+    }
+    
+    
+    public void showIMAP(IMap<String, List<String>> tempList, Boolean isSort) {
+
+        if (isSort) {
+            Collections.sort(categories);
+        }
+
+        for (int i = 0; i < categories.size(); i++) {
+
+            List<String> products = tempList.get(categories.get(i));
+
+            if (products != null) {
+                System.out.println("\n-> " + categories.get(i));
+
+                for (int e = 0; e < products.size(); e++) {
+                    System.out.println("  - " + products.get(e) + "| 1");
+                }
+
+            }
+        }
+
+      
+
+    }
+
+    /*
+     * Busca el producto en el inventario y muestra de que categoria es
+     */
+    public void showCategoryOfAProduct() throws IOException {
+        System.out.println("\n :: MOSTRAR CATEGORIA DE UN PRODUCTO ::\n");
+
+        System.out.print("-> Ingrese el nombre de un producto: ");
+        String product = br.readLine();
+
+        String tempCategory = "";
+
+        for (int i = 0; i < categories.size(); i++) {
+
+            List<String> products = inventary.get(categories.get(i));
+
+            for (int e = 0; e < products.size(); e++) {
+                if (products.get(e).equals(product)) {
+                    tempCategory = categories.get(i);
+                }
+            }
+
+        }
+
+        System.out.println("\n [OK] El producto ingresado pertence a la categoria: ");
+        System.out.println("         -> " + tempCategory + "\n");
+        
+
+    }
+
+    /*
+     * Espera a que el usuario presione una tecla para continuar la ejecución
+     */
+    
     
     
     
--- HDT6/src/hdt6/HDT6.java
+++ HDT6/src/hdt6/HDT6.java
@@ -4,8 +4,11 @@
  */
 package hdt6;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 /**
  *
@@ -22,10 +25,62 @@ public class HDT6 {
         File Archivo;
         Archivo = new File("ListadoProducto.txt");
         Controlador dat = new Controlador();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         System.out.println("Bienvenido a la tiendita online ");
-        //si no existe ningún archivo, este lo crea. De lo contrario procede a operar
+        
         dat.leerOperaciones(Archivo);
+        dat.Factory();
+        
+        IMap<String, List<String>> inventario = dat.getInventary();
+        IMap<String, List<String>> colleccion = dat.getCollection();
+        
+        boolean Terminar = true;
+
+        while (Terminar) {
+            
+
+            System.out.println("1. Agregar producto a la collecion");
+            System.out.println("2. Mostrar categoria de un producto");
+            System.out.println("3. Mostrar coleccion del usuario");
+            System.out.println("4. Mostrar coleccion del usuario (Ordenados por categoria)");
+            System.out.println("5. Mostrar inventario");
+            System.out.println("6. Mostrar inventario (Ordenados por categoria)");
+            System.out.println("7. Salir");
+            Integer option = Integer.parseInt(br.readLine());
+
+            if (option == 1) {
+
+                dat.addProductToCollection();
+
+            } else if (option == 2) {
+                dat.showCategoryOfAProduct();
+
+            } else if (option == 3) {
+                System.out.println("\n :: COLECCION ::\n");
+                dat.showIMAP(colleccion, false);
+
+            } else if (option == 4) {
+                System.out.println("\n :: COLECCION ORDENADA POR CATEGORIA ::\n");
+                dat.showIMAP(colleccion, true);
+
+            } else if (option == 5) {
+
+                System.out.println("\n :: INVENTARIO ::\n");
+                dat.showIMAP(colleccion, false);
+
+            } else if (option == 6) {
+                System.out.println("\n :: INVENTARIO ORDENADA POR CATEGORIA  ::\n");
+                dat.showIMAP(colleccion, true);
+
+            } else if (option == 7) {
+
+                Terminar = false;
+                System.out.println("Finalizando programa ...");
+                break;
+            }
+
+        }
         
     }
     
